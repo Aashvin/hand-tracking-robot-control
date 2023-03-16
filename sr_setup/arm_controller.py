3b38cd0829d30d9b12898225f6bdca623a2d7ec2
Aashvin <55795472+Aashvin@users.noreply.github.com>
2023-03-16 10:27:53 +0000

    Added support for arm control

--- sr_setup/arm_controller.py
+++ sr_setup/arm_controller.py
@@ -9,80 +9,127 @@ from sr_robot_commander.sr_arm_commander import SrArmCommander
 import tf
 
 from robot_controller import RobotController
+from webcam_controller import HAND_LANDMARKS
 
-    #         # Get the position of the base of the middle finger for the x, y of the robot
-    #         mid_finger_x = hand.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_MCP].x
-    #         mid_finger_y = hand.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y
-    #         mid_finger_pos = (mid_finger_x, mid_finger_y)
+#         # Get the position of the base of the middle finger for the x, y of the robot
+#         mid_finger_x = hand.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_MCP].x
+#         mid_finger_y = hand.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y
+#         mid_finger_pos = (mid_finger_x, mid_finger_y)
 
-    #         # Get the normalised distance between the base of the middle finger and the wrist for the z of the robot
-    #         squared_y_dist = (
-    #             hand.landmark[self.mp_hands.HandLandmark.WRIST].y
-    #             - hand.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y
-    #         ) ** 2
-    #         squared_x_dist = (
-    #             hand.landmark[self.mp_hands.HandLandmark.WRIST].x
-    #             - hand.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_MCP].x
-    #         ) ** 2
-    #         norm_dist = np.sqrt(squared_y_dist + squared_x_dist)
+#         # Get the normalised distance between the base of the middle finger and the wrist for the z of the robot
+#         squared_y_dist = (
+#             hand.landmark[self.mp_hands.HandLandmark.WRIST].y
+#             - hand.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y
+#         ) ** 2
+#         squared_x_dist = (
+#             hand.landmark[self.mp_hands.HandLandmark.WRIST].x
+#             - hand.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_MCP].x
+#         ) ** 2
+#         norm_dist = np.sqrt(squared_y_dist + squared_x_dist)
 
 
 class ArmController(RobotController):
     def __init__(self, name: str) -> None:
         super().__init__()
-        self.controller = SrArmCommander(name=name)
+        self.commander = SrArmCommander(name=name)
+        self.orientation = None
+        self.constraints = None
 
-    def set_required_data(self):
-        self.required_data = 
+    def set_required_landmarks(self):
+        self.required_landmarks = [
+            HAND_LANDMARKS.MIDDLE_FINGER_MCP,
+            HAND_LANDMARKS.WRIST,
+        ]
+
+    def set_constraints(self):
+        self.constraints = moveit_msgs.msg.Constraints()
+        self.constraints.name = "all_constraints"
+
+        shoulder_list_constraint = moveit_msgs.msg.JointConstraint()
+        shoulder_list_constraint.joint_name = "ra_shoulder_lift_joint"
+        shoulder_list_constraint.position = np.radians(-45)
+        shoulder_list_constraint.tolerance_below = np.radians(45)
+        shoulder_list_constraint.tolerance_above = np.radians(100)
+
+        self.constraints.joint_constraints = [shoulder_list_constraint]
 
     def move_to_start_pose(self):
-        starting_pose = self.controller.get_current_pose("ra_base")
+        self.commander.set_pose_reference_frame("ba_base")
+        self.commander.move_to_named_target("ra_start")
+        self.commander.move_to_joint_value_target_unsafe(
+            {"ra_wrist_3_joint": -180}, wait=True, angle_degrees=True
+        )
 
-        desired_pose = geometry_msgs.msg.PoseStamped()
+        current_pose = self.commander.get_current_pose("ra_base")
 
-        desired_pose.pose.position.x = 0.5
-        desired_pose.pose.position.y = -0.5
-        desired_pose.pose.position.z = starting_pose.position.z
+        starting_pose = geometry_msgs.msg.PoseStamped()
+
+        starting_pose.pose.position.x = 0.5
+        starting_pose.pose.position.y = -0.5
+        starting_pose.pose.position.z = current_pose.position.z
 
         starting_orientation_euler = tf.transformations.euler_from_quaternion(
             (
-                starting_pose.orientation.x,
-                starting_pose.orientation.y,
-                starting_pose.orientation.z,
-                starting_pose.orientation.w,
+                current_pose.orientation.x,
+                current_pose.orientation.y,
+                current_pose.orientation.z,
+                current_pose.orientation.w,
             )
         )
 
-        desired_orientation = tf.transformations.quaternion_from_euler(
+        starting_orientation = tf.transformations.quaternion_from_euler(
             starting_orientation_euler[0], starting_orientation_euler[1], 0
         )
 
-        desired_pose.pose.orientation.x = desired_orientation[0]
-        desired_pose.pose.orientation.y = desired_orientation[1]
-        desired_pose.pose.orientation.z = desired_orientation[2]
-        desired_pose.pose.orientation.w = desired_orientation[3]
-        self.controller.move_to_pose_value_target_unsafe(desired_pose)
+        starting_pose.pose.orientation.x = starting_orientation[0]
+        starting_pose.pose.orientation.y = starting_orientation[1]
+        starting_pose.pose.orientation.z = starting_orientation[2]
+        starting_pose.pose.orientation.w = starting_orientation[3]
 
-        return desired_pose
+        self.orientation = starting_pose.pose.orientation
+
+        self.set_constraints()
+
+        self.commander.move_to_pose_value_target_unsafe(starting_pose)
+
+    def process_landmark_data(self, landmark_data):
+        mid_finger_x = landmark_data["x"][HAND_LANDMARKS.MIDDLE_FINGER_MCP]
+        mid_finger_y = landmark_data["y"][HAND_LANDMARKS.MIDDLE_FINGER_MCP]
+        hand_x_y = (mid_finger_x, mid_finger_y)
+
+        squared_x_dist = (
+            landmark_data["x"][HAND_LANDMARKS.WRIST]
+            - landmark_data["x"][HAND_LANDMARKS.MIDDLE_FINGER_MCP]
+        ) ** 2
+        squared_y_dist = (
+            landmark_data["y"][HAND_LANDMARKS.WRIST]
+            - landmark_data["y"][HAND_LANDMARKS.MIDDLE_FINGER_MCP]
+        ) ** 2
+        norm_dist = np.sqrt(squared_x_dist * squared_y_dist)
+
+        arm_position_dict = {
+            "x": -mid_finger_y + 1,
+            "y": -mid_finger_x * 2 + 1,
+            "z": max(0.2, 0.8 - norm_dist),
+        }
+
+        return arm_position_dict
 
     def publish_move(self):
-        constraint = moveit_msgs.msg.Constraints()
-        constraint.name = "all_constraints"
-        arm_constraint = moveit_msgs.msg.JointConstraint()
-        arm_constraint.joint_name = "ra_shoulder_lift_joint"
-        arm_constraint.position = np.radians(-45)
-        arm_constraint.tolerance_below = np.radians(45)
-        arm_constraint.tolerance_above = np.radians(100)
-
-        constraint.joint_constraints = [arm_constraint]
-
         while True:
-            move = self.pub_queue.get()
+            arm_position_dict = self.pub_queue.get()
 
-            if move == "END":
+            if arm_position_dict == "END":
                 return
 
-            if move is not None:
-                self.controller.move_to_pose_value_target_unsafe(
-                    move, wait=False, ik_constraints=constraint
+            if arm_position_dict is not None:
+                desired_pose = geometry_msgs.msg.PoseStamped()
+                desired_pose.pose.orientation = self.orientation
+
+                desired_pose.pose.position.x = arm_position_dict["x"]
+                desired_pose.pose.position.y = arm_position_dict["y"]
+                desired_pose.pose.position.z = arm_position_dict["z"]
+
+                self.commander.move_to_pose_value_target_unsafe(
+                    desired_pose, wait=False, ik_constraints=self.constraints
                 )
--- sr_setup/system_controller.py
+++ sr_setup/system_controller.py
@@ -99,69 +99,35 @@ class Controller:
             min_detection_confidence=0.7, min_tracking_confidence=0.7
         ) as hands:
             while self.webcam_controller.cap.isOpened():
-                ret, frame = self.webcam_controller.cap.read()
-
-                # Convert from BGR to RGB
-                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-                # Flip on the horizontal axis for hand tracking and display
-                image = cv2.flip(image, 1)
-
-                # Set flag
-                image.flags.writeable = False
-
-                # Hand tracking for this frame
-                results = hands.process(image)
-
-                # Set flag to true
-                image.flags.writeable = True
-
-                # RGB 2 BGR
-                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                image, results = self.webcam_controller.read_capture(hands)
 
                 # Rendering results
                 if results.multi_hand_landmarks:
-                    for num, hand in enumerate(results.multi_hand_landmarks):
-                        self.webcam_controller.mp_drawing.draw_landmarks(
-                            image,
-                            hand,
-                            self.webcam_controller.mp_hands.HAND_CONNECTIONS,
-                        )
+                    self.webcam_controller.draw_landmark_results(results, image)
 
-                    # Draw angles to image from joint list
-                    (
-                        angle_list,
-                        image,
-                        mid_finger_pos,
-                        norm_dist,
-                    ) = self.webcam_controller.finger_angles(image, results)
+                    hand_landmark_data = self.webcam_controller.get_landmark_data(
+                        results, self.hand_controller.required_landmarks
+                    )
+                    arm_landmark_data = self.webcam_controller.get_landmark_data(
+                        results, self.arm_controller.required_landmarks
+                    )
+
+                    angle_dict = self.hand_controller.finger_angles(hand_landmark_data)
+
+                    arm_position_dict = self.arm_controller.process_landmark_data(
+                        arm_landmark_data
+                    )
+
+                    self.webcam_controller.display_angle(
+                        image, hand_landmark_data, angle_dict
+                    )
 
                     time2 = time.time()
                     if time2 - time1 > 0.5:
                         time1 = time2
 
-                        flex = {
-                            "rh_FFJ2": angle_list[1],
-                            "rh_FFJ3": angle_list[2],
-                            "rh_MFJ2": angle_list[3],
-                            "rh_MFJ3": angle_list[4],
-                            "rh_RFJ2": angle_list[5],
-                            "rh_RFJ3": angle_list[6],
-                            "rh_LFJ2": angle_list[7],
-                            "rh_LFJ3": angle_list[8],
-                            "rh_THJ2": angle_list[9],
-                            "rh_THJ3": angle_list[10],
-                        }
-
-                        self.hand_controller.pub_queue.put(flex)
-
-                        desired_pose.pose.position.x = -mid_finger_pos[1] + 1
-                        desired_pose.pose.position.y = -mid_finger_pos[0] * 2 + 1
-                        desired_pose.pose.position.z = 0.4 - norm_dist
-                        desired_pose.pose.position.z = max(
-                            0.2, desired_pose.pose.position.z
-                        )
-                        self.arm_controller.pub_queue.put(desired_pose)
+                        self.hand_controller.data_queue.put(angle_dict)
+                        self.arm_controller.data_queue.put(arm_position_dict)
 
                 # Showing the camera
                 cv2.imshow("Finger Angles", image)
