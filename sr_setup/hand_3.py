075752de96a446c403341c59fbfeebbe1831e3c1
Aashvin <55795472+Aashvin@users.noreply.github.com>
2023-03-19 16:45:41 +0000

    Added 3 finger hand support to new framework

---
+++ sr_setup/hand_3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3.8
+
+import rospy
+
+from arm_controller import ArmController
+from hand_3_controller import HandController
+from webcam_controller import WebcamController
+from system_controller import Controller
+
+
+def run():
+    rospy.init_node("hand_controller", anonymous=True)
+
+    hand = HandController()
+    cam = WebcamController()
+
+    controller = Controller(webcam_controller=cam, hand_controller=hand)
+
+    controller.run_hand()
+
+
+if __name__ == "__main__":
+    run()
