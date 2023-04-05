9a064955501f6205704adaed78ca8f5ee004e327
Aashvin <55795472+Aashvin@users.noreply.github.com>
2023-04-05 19:02:40 +0100

    edited setup and changed test crlf to lf

--- sr_setup.py
+++ sr_setup.py
@@ -1,5 +1,7 @@
 import os
 
+print("Please don't close the pop-up window.")
+
 os.system("docker start dexterous_hand_simulated")
 
 os.system("docker cp sr_setup.sh dexterous_hand_simulated:/home/user/sr_setup.sh")
@@ -25,9 +27,17 @@ os.system(
 )
 
 os.system(
-    'docker exec -ti --user user -w /home/user dexterous_hand_simulated /bin/bash -c "python3.8 -m pip install mediapipe"'
+    'docker exec -ti --user user -w /home/user dexterous_hand_simulated /bin/bash -c "chmod +x /home/user/projects/shadow_robot/base/src/robot_control/src/scripts/test_hand.py"'
+)
+
+os.system(
+    'docker exec -ti --user user -w /home/user dexterous_hand_simulated /bin/bash -c "python3.8 -m pip install mediapipe pandas"'
 )
 
 os.system(
     'docker exec -ti --user user -w /home/user dexterous_hand_simulated /bin/bash -c "rm /home/user/sr_setup.sh"'
 )
+
+os.system("docker stop dexterous_hand_simulated")
+
+print("Shadow Robot setup done!")
